248bad82c72bb4a3db45a0feabd52e3d6cd74222
Andy Byers <a.byers@bbk.ac.uk>
2024-09-05 16:36:09 +0100

    Updates plugin for 1.7 release.

--- plugin_settings.py
+++ plugin_settings.py
@@ -4,10 +4,10 @@ PLUGIN_NAME = 'Library of Congress Plugin'
 DISPLAY_NAME = 'Library of Congress'
 DESCRIPTION = 'Libray of Congress Deposit Plugin'
 AUTHOR = 'Andy Byers'
-VERSION = '0.1'
+VERSION = '0.2'
 SHORT_NAME = 'loc_transporter'
 MANAGER_URL = 'loc_transporter_manager'
-JANEWAY_VERSION = "1.4.2"
+JANEWAY_VERSION = "1.7.0"
 
 
 class LOCDepositPlugin(plugins.Plugin):
